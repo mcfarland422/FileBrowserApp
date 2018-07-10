09ca73231fa944a8adc0da07baf61cf79b11c220
Michael McFarland <gardengrove422@gmail.com>
2018-07-10 19:58:13 -0400

    Folders and Icons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { File } from "@ionic-native/file";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  private dirs: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private fileCtrl: File) {
+    this.goToDir();
   }
 
+  public goToDir() {
+    this.fileCtrl
+      .listDir(this.fileCtrl.externalRootDirectory, "")
+      .then(list => {
+        this.dirs = list;
+      });
+  }
 }
